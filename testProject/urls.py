c4b9433aad4d4f9d0c875d351ec0dd82be8cfe18
Volodimer <gaemvova3@gmail.com>
2026-05-08 18:56:01 +0600

    Project Upgrade

---
+++ testProject/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from testProject import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:article_id>/', views.get_article, name='article'),
+    path('about/', views.about, name='about'),
+]
